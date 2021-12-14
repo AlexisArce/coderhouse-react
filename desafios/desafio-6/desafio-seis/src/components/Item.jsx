048e32e22d007488811bdc4cd6a79f1749d24617
aarce <Arce.Alexis@hotmail.com>
2021-12-14 12:58:51 -0300

    Update Item component styles

--- desafios/desafio-6/desafio-seis/src/components/Item.jsx
+++ desafios/desafio-6/desafio-seis/src/components/Item.jsx
@@ -7,14 +7,18 @@ const Item = ({ item }) => {
         src={item.image}
         className="card-img-top p-2"
         alt={item.title}
-        max-width="150"
-        max-height="300"
+        width="300"
+        height="300"
       />
       <div className="card-body">
         <h5 className="card-title">{item.title}</h5>
-        <p className="card-text">{item.description}</p>
+        <p className="card-text badge bg-secondary">$ {item.price}</p>
         <ItemCounter min={1} max={5} stock={100} />
-        <button className="btn btn-primary">Agregar</button>
+        <div className="d-grid gap-2">
+          <button class="btn btn-primary" type="button">
+            Agregar al carrito
+          </button>
+        </div>
       </div>
     </div>
   );
